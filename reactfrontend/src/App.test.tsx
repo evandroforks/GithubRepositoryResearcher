d62e7c912d532c8aa885364bde4e0d920686683d
evandrocoan <evandrocoan@hotmail.com>
2020-02-07 23:10:41 -0300

    Added ryanjyost/react-responsive-tutorial as template for
    the reactfrontend.

--- reactfrontend/src/App.test.tsx
+++ reactfrontend/src/App.test.tsx
@@ -3,13 +3,11 @@ import { render } from '@testing-library/react';
 import App from './App';
 import ReactDOM from 'react-dom';
 
-
-test('renders learn react link', () => {
+test('renders GitHub Repository Researcher site name', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/GH RRS/i);
   expect(linkElement).toBeInTheDocument();
 });
-
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
--- reactfrontend/src/App.tsx
+++ reactfrontend/src/App.tsx
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TopBar from "./components/TopBar";
+import FooterMenu from "./components/FooterMenu";
+import Content from "./components/Content";
+import Sidebar from "./components/Sidebar";
 
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppProps {
+}
+
+interface AppState {
+  windowWidth: number,
+  windowHeight: number
+}
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      windowWidth: 0,
+      windowHeight: 0
+    };
+  }
+
+  componentDidMount() {
+    this.updateDimensions();
+    window.addEventListener("resize", this.updateDimensions.bind(this));
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions.bind(this));
+  }
+
+  updateDimensions() {
+    let windowWidth = typeof window !== "undefined" ? window.innerWidth : 0;
+    let windowHeight = typeof window !== "undefined" ? window.innerHeight : 0;
+
+    this.setState({ windowWidth, windowHeight });
+  }
+
+  render() {
+    const { windowWidth } = this.state;
+
+    const styles = {
+      white: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      black: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+      topBarHeight: 40,
+      footerMenuHeight: 50,
+      showFooterMenuText: windowWidth > 500,
+      showSidebar: windowWidth > 768,
+      sidebarWidth: windowWidth < 1100 ? 100 : 200,
+      sidebarCollapsed: windowWidth < 1100
+    };
+
+    const menuItems = styles.showSidebar
+      ? [
+        { icon: `😀`, text: "Item 1" },
+        { icon: `😉`, text: "Item 2" },
+        { icon: `😎`, text: "Item 3" },
+        { icon: `🤔`, text: "Item 4" },
+        { icon: `😛`, text: "Item 5" },
+        { icon: `😺️`, text: "Profile" },
+        { icon: `⚙`, text: "Settings" }
+      ]
+      : [
+        { icon: `😀`, text: "Item 1" },
+        { icon: `😉`, text: "Item 2" },
+        { icon: `😎`, text: "Item 3" },
+        { icon: `🤔`, text: "Item 4" },
+        { icon: `😛`, text: "Item 5" }
+      ];
+
+    return (
+      <div
+        style={{
+          backgroundColor: styles.black(0.05),
+          minHeight: "100vh",
+          position: "relative"
+        }}
+      >
+        {styles.showSidebar ? (
+          <Sidebar menuItems={menuItems} styles={styles} />
+        ) : (
+            <TopBar styles={styles} />
+          )}
+
+        <Content styles={styles} />
+
+        {!styles.showSidebar && (
+          <FooterMenu menuItems={menuItems} styles={styles} />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
